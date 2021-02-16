98b8bf49130495346443c3a9ef999c3364836206
Lucas Saraiva <lucas.saraiva019@gmail.com>
2021-02-16 16:56:23 -0300

    refactor: it refactors the find tests.

--- pkg/arango_find_test.go
+++ pkg/arango_find_test.go
@@ -10,6 +10,21 @@ import (
 var _ = Describe("ArangoDB Find", func() {
 	var user User
 
+	fnCreateUser := func(name, email string) User {
+		newUser := &User{
+			Name:  name,
+			Email: email,
+		}
+		tx := gormDB.Create(newUser)
+		Expect(tx).NotTo(BeNil())
+		Expect(tx.Error).To(BeNil())
+		Expect(tx.Statement.Dest).NotTo(BeNil())
+
+		user := tx.Statement.Dest.(*User)
+		Expect(user.ID).NotTo(BeZero())
+		return *user
+	}
+
 	var _ = BeforeEach(func() {
 		By("preparing collection", func() {
 			err := gormDB.AutoMigrate(&User{})
@@ -17,17 +32,7 @@ var _ = Describe("ArangoDB Find", func() {
 		})
 
 		By("creating registers", func() {
-			newUser := &User{
-				Name:  "Joselito",
-				Email: "joselitofilhoo@gmail.com",
-			}
-			tx := gormDB.Create(newUser)
-			Expect(tx).NotTo(BeNil())
-			Expect(tx.Error).To(BeNil())
-			Expect(tx.Statement.Dest).NotTo(BeNil())
-
-			user = *tx.Statement.Dest.(*User)
-			Expect(user.ID).NotTo(BeZero())
+			user = fnCreateUser("Joselito", "joselitofilhoo@gmail.com")
 		})
 	})
 
@@ -36,7 +41,7 @@ var _ = Describe("ArangoDB Find", func() {
 		Expect(err).NotTo(HaveOccurred())
 	})
 
-	It("retrieves the user by id", func() {
+	It("retrieves the user by ID", func() {
 		var getUser User
 		tx := gormDB.Find(&getUser, fmt.Sprintf("{\"ID\": %d}", user.ID))
 		Expect(tx).NotTo(BeNil())
@@ -47,5 +52,45 @@ var _ = Describe("ArangoDB Find", func() {
 		Expect(getUser.CreatedAt).NotTo(BeZero())
 		Expect(getUser.UpdatedAt).NotTo(BeZero())
 		Expect(getUser.DeletedAt).To(BeZero())
+	})
+
+	When("operator is $eq", func() {
+		It("returns user to the passed user.ID", func() {
+			var getUser User
+			tx := gormDB.Find(&getUser, fmt.Sprintf("{\"ID\": {\"$eq\": %d}}", user.ID))
+			Expect(tx).NotTo(BeNil())
+			Expect(tx.Error).To(BeNil())
+			Expect(getUser.ID).To(Equal(user.ID))
+		})
+	})
+
+	When("operator is $gt", func() {
+		It("returns the first user with an ID greater than zero", func() {
+			var getUser User
+			tx := gormDB.Find(&getUser, fmt.Sprintf("{\"ID\": {\"$gt\": %d}}", 0))
+			Expect(tx).NotTo(BeNil())
+			Expect(tx.Error).To(BeNil())
+			Expect(getUser.ID).To(Equal(user.ID))
+		})
+	})
+
+	When("user.ID passed does not exist in the database", func() {
+		It("returns a 'not found' error", func() {
+			var getUser User
+			tx := gormDB.Find(&getUser, fmt.Sprintf("{\"ID\": %d}", user.ID+1))
+			Expect(tx).NotTo(BeNil())
+			Expect(tx.Error).NotTo(BeNil())
+			Expect(tx.Error.Error()).To(Equal("Document not found"))
+		})
+	})
+
+	When("ID passed is invalid", func() {
+		It("returns a 'not found' error", func() {
+			var getUser User
+			tx := gormDB.Find(&getUser, fmt.Sprintf("{\"ID\": %d}", -1))
+			Expect(tx).NotTo(BeNil())
+			Expect(tx.Error).NotTo(BeNil())
+			Expect(tx.Error.Error()).To(Equal("Document not found"))
+		})
 	})
 })
