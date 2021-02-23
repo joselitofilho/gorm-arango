aae48ccd862b391b8ff14ac3811cf3323e0628b9
Joselito Viveiros Nogueira Filho <joselitofilhoo@gmail.com>
2021-02-23 02:01:47 +0000

    chore: method list returns a slice or single instance.

--- internal/callbacks/query.go
+++ internal/callbacks/query.go
@@ -1,19 +1,55 @@
 package callbacks
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"time"
+
 	"github.com/joselitofilho/gorm/driver/arango/internal/conn"
+	"github.com/joselitofilho/gorm/driver/arango/internal/errors"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
+
+func newInstanceOfSliceType(arr interface{}) reflect.Type {
+	val := reflect.ValueOf(arr)
+	if val.Kind() == reflect.Ptr {
+		return newInstanceOfSliceType(val.Elem().Interface())
+	}
+	if val.Kind() != reflect.Slice {
+		// TODO: Implement
+		return nil
+	}
+	return reflect.TypeOf(arr).Elem()
+}
 
 func Query(db *gorm.DB) {
 	if db.Error == nil {
 		buildAQL(db)
 
-		db.Statement.ConnPool.(*conn.ConnPool).Result = db.Statement.Dest
+		elemType := newInstanceOfSliceType(db.Statement.Dest)
+
+		isSlice := db.Statement.ReflectValue.Kind() == reflect.Slice || db.Statement.ReflectValue.Kind() == reflect.Array
+		db.Statement.ConnPool.(*conn.ConnPool).Return = conn.ConnPoolReturn{
+			Dest:     db.Statement.Dest,
+			ElemType: elemType,
+			IsSlice:  isSlice,
+		}
+
 		_, err := db.Statement.ConnPool.QueryContext(db.Statement.Context, db.Statement.SQL.String(), db.Statement.Vars...)
 		if err != nil {
 			db.AddError(err)
 			return
+		}
+
+		if isSlice {
+			model := reflect.New(elemType).Interface()
+			if err := scan(db, model); err != nil {
+				db.AddError(err)
+				return
+			}
 		}
 	}
 }
@@ -27,4 +63,123 @@ func buildAQL(db *gorm.DB) {
 
 	// TODO: select.
 	db.Statement.SQL.WriteString(" RETURN doc")
+
+}
+
+func rowColumns(model interface{}) ([]string, error) {
+	data, err := json.Marshal(model)
+	if err != nil {
+		return nil, err
+	}
+
+	var mapModel map[string]interface{}
+	err = json.Unmarshal(data, &mapModel)
+	if err != nil {
+		return nil, err
+	}
+
+	columns := make([]string, 0)
+	for key := range mapModel {
+		columns = append(columns, key)
+	}
+
+	return columns, nil
+}
+
+// This method is based on gorm.Scan() method.
+func scan(db *gorm.DB, model interface{}) error {
+	db.RowsAffected = 0
+
+	columns, err := rowColumns(model)
+	if err != nil {
+		return err
+	}
+
+	values := make([]interface{}, len(columns))
+
+	switch dest := db.Statement.Dest.(type) {
+	case map[string]interface{}, *map[string]interface{},
+		*[]map[string]interface{},
+		*int, *int8, *int16, *int32, *int64,
+		*uint, *uint8, *uint16, *uint32, *uint64, *uintptr,
+		*float32, *float64,
+		*bool, *string, *time.Time,
+		*sql.NullInt32, *sql.NullInt64, *sql.NullFloat64,
+		*sql.NullBool, *sql.NullString, *sql.NullTime:
+		return errors.ErrMethodNotImplemented(dest)
+	default:
+		Schema := db.Statement.Schema
+
+		switch db.Statement.ReflectValue.Kind() {
+		case reflect.Slice, reflect.Array:
+			var (
+				reflectValueType = db.Statement.ReflectValue.Type().Elem()
+				isPtr            = reflectValueType.Kind() == reflect.Ptr
+				fields           = make([]*schema.Field, len(columns))
+			)
+
+			if isPtr {
+				reflectValueType = reflectValueType.Elem()
+			}
+
+			db.Statement.ReflectValue.Set(reflect.MakeSlice(db.Statement.ReflectValue.Type(), 0, 20))
+
+			if Schema != nil {
+				if reflectValueType != Schema.ModelType && reflectValueType.Kind() == reflect.Struct {
+					return fmt.Errorf("Invalid schema %v.", Schema)
+				}
+
+				for idx, column := range columns {
+					if field := Schema.LookUpField(column); field != nil && field.Readable {
+						fields[idx] = field
+					} else {
+						values[idx] = &sql.RawBytes{}
+					}
+				}
+			}
+
+			// pluck values into slice of data
+			isPluck := false
+			if len(fields) == 1 {
+				if _, ok := reflect.New(reflectValueType).Interface().(sql.Scanner); ok || // is scanner
+					reflectValueType.Kind() != reflect.Struct || // is not struct
+					Schema.ModelType.ConvertibleTo(schema.TimeReflectType) { // is time
+					isPluck = true
+				}
+			}
+
+			for _, row := range db.Statement.ConnPool.(*conn.ConnPool).Return.Dest.([]interface{}) {
+				db.RowsAffected++
+
+				data, _ := json.Marshal(row)
+				var mapModel map[string]interface{}
+				json.Unmarshal(data, &mapModel)
+
+				elem := reflect.New(reflectValueType)
+				if isPluck {
+					db.AddError(fmt.Errorf("Error scanning row: %v", row))
+				} else {
+					for idx, field := range fields {
+						if field != nil {
+							values[idx] = mapModel[field.Name]
+						}
+					}
+
+					for idx, field := range fields {
+						if field != nil {
+							field.Set(elem, values[idx])
+						}
+					}
+				}
+
+				if isPtr {
+					db.Statement.ReflectValue.Set(reflect.Append(db.Statement.ReflectValue, elem))
+				} else {
+					db.Statement.ReflectValue.Set(reflect.Append(db.Statement.ReflectValue, elem.Elem()))
+				}
+			}
+		}
+	}
+
+	return nil
 }
--- internal/conn/connpoll.go
+++ internal/conn/connpoll.go
@@ -4,15 +4,22 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"reflect"
 
 	driver "github.com/arangodb/go-driver"
 	"github.com/joselitofilho/gorm/driver/arango/internal/transformers"
 )
 
+type ConnPoolReturn struct {
+	Dest     interface{}
+	ElemType reflect.Type
+	IsSlice  bool
+}
+
 type ConnPool struct {
 	Connection driver.Connection
 	Database   driver.Database
-	Result     interface{}
+	Return     ConnPoolReturn
 }
 
 func (connPool *ConnPool) PrepareContext(ctx context.Context, query string) (*sql.Stmt, error) {
@@ -33,12 +40,30 @@ func (connPool *ConnPool) QueryContext(ctx context.Context, query string, args .
 	}
 	defer cursor.Close()
 
-	_, err = cursor.ReadDocument(ctx, connPool.Result)
+	if connPool.Return.IsSlice {
+		results := make([]interface{}, 0)
+		for {
+			var r interface{}
+			r = reflect.New(connPool.Return.ElemType).Interface()
+			_, err = cursor.ReadDocument(ctx, r)
 
-	if driver.IsNoMoreDocuments(err) {
-		return nil, errors.New("Document not found")
-	} else if err != nil {
-		return nil, err
+			if driver.IsNoMoreDocuments(err) {
+				break
+			} else if err != nil {
+				return nil, err
+			} else {
+				results = append(results, r)
+			}
+		}
+		connPool.Return.Dest = results
+	} else {
+		_, err = cursor.ReadDocument(ctx, connPool.Return.Dest)
+
+		if driver.IsNoMoreDocuments(err) {
+			return nil, errors.New("Document not found")
+		} else if err != nil {
+			return nil, err
+		}
 	}
 
 	// TODO: need implements parse to *sql.Rows?
